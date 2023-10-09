08729a7b9ed0c8871a82f4573d6a14a67cc132a3
Aytac Serce <aytacserce@gmail.com>
2023-10-09 16:52:43 +0300

    Add files via upload

---
+++ src/ui/ProtectedRoute.jsx
@@ -0,0 +1,42 @@
+/* eslint-disable react/prop-types */
+import styled from "styled-components";
+import { useUser } from "../features/authentication/useUser";
+import { useNavigate } from "react-router-dom";
+import Spinner from "./Spinner";
+import { useEffect } from "react";
+
+const FullPage = styled.div`
+  height: 100vh;
+  background-color: var(--color-grey-50);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+function ProtectedRoute({ children }) {
+  const navigate = useNavigate();
+
+  // 1. Load authenticated user
+  const { isAuthenticated, isLoading } = useUser();
+
+  // 2. If no auth user, redirect to login
+  useEffect(
+    function () {
+      if (!isAuthenticated && !isLoading) navigate("/login");
+    },
+    [isAuthenticated, isLoading, navigate]
+  );
+
+  // 3. Show spinner while loading
+  if (isLoading)
+    return (
+      <FullPage>
+        <Spinner />
+      </FullPage>
+    );
+
+  // 4. If there is a user, render the app
+  if (isAuthenticated) return children;
+}
+
+export default ProtectedRoute;
